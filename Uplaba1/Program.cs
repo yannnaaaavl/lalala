83de639722dc31e1dd2b6ab8d80e9f5a53f86ef2
Зискин Илья Игоревич <322112@college>
2025-03-17 10:02:36 +0300

    Initial commit: создан класс Company

---
+++ Uplaba1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uplaba1
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int YearFounded { get; set; }
+
+        public Company(string name, string address, int yearFounded)
+        {
+            Name = name;
+            Address = address;
+            YearFounded = yearFounded;
+        }
+
+        //public void DisplayInfo()
+        //{
+        //    Console.WriteLine($"Компания: {Name}");
+        //    Console.WriteLine($"Адрес: {Address}");
+        //    Console.WriteLine($"Год основания: {YearFounded}");
+        //}
+
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+                {
+                    //Company company = new Company(" АААААА", "г. Полоцк, ул. Октябрьская, д. 57", 2000);
+                    //company.DisplayInfo();
+                }
+            }
+    }
+}
+   
+}
---
+++ Uplaba1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Uplaba1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Uplaba1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1ec942e0-31c0-4b7f-82b6-a86f41a6642f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
